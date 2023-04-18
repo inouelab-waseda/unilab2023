6a00819fde8ad4afa7f8cd4fe0ae5153943f41d4
shinya <gshinya@akane.waseda.jp>
2023-04-18 15:53:45 +0900

    キャラクターのマスを動かすプログラム

---
+++ pre_unilab/pre_unilab/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace pre_unilab
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(54, 71);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(259, 244);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Location = new System.Drawing.Point(77, 372);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(446, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 279);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(450, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.AllowDrop = true;
+            this.listBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Items.AddRange(new object[] {
+            "up",
+            "down",
+            "right",
+            "left"});
+            this.listBox2.Location = new System.Drawing.Point(54, 334);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(150, 104);
+            this.listBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private ListBox listBox2;
+    }
+}
---
+++ pre_unilab/pre_unilab/Form1.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace pre_unilab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.SelectionMode = SelectionMode.One;
+
+            //ListBox1のイベントハンドラを追加
+            listBox2.MouseDown += new MouseEventHandler(ListBox2_MouseDown);
+            //ListBox2のイベントハンドラを追加
+            listBox1.DragEnter += new DragEventHandler(ListBox1_DragEnter);
+            listBox1.DragDrop += new DragEventHandler(ListBox1_DragDrop);
+
+        }
+
+        //ListBox1でマウスボタンが押された時
+        private void ListBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            //マウスの左ボタンだけが押されている時のみドラッグできるようにする
+            if (e.Button == MouseButtons.Left)
+            {
+                //ドラッグの準備
+                ListBox lbx = (ListBox)sender;
+                //ドラッグするアイテムのインデックスを取得する
+                int itemIndex = lbx.IndexFromPoint(e.X, e.Y);
+                if (itemIndex < 0) return;
+                //ドラッグするアイテムの内容を取得する
+                string itemText = (string)lbx.Items[itemIndex];
+
+                //ドラッグ&ドロップ処理を開始する
+                DragDropEffects dde =
+                    lbx.DoDragDrop(itemText, DragDropEffects.All);
+
+                ////ドロップ効果がMoveの時はもとのアイテムを削除する
+                //if (dde == DragDropEffects.Move)
+                //    lbx.Items.RemoveAt(itemIndex);
+            }
+        }
+
+        //ListBox2内にドラッグされた時
+        private void ListBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            //ドラッグされているデータがstring型か調べ、
+            //そうであればドロップ効果をMoveにする
+            if (e.Data.GetDataPresent(typeof(string)))
+                e.Effect = DragDropEffects.Move;
+            else
+                //string型でなければ受け入れない
+                e.Effect = DragDropEffects.None;
+        }
+
+        //ListBox2にドロップされたとき
+        private void ListBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            //ドロップされたデータがstring型か調べる
+            if (e.Data.GetDataPresent(typeof(string)))
+            {
+                ListBox target = (ListBox)sender;
+                //ドロップされたデータ(string型)を取得
+                string itemText =
+                    (string)e.Data.GetData(typeof(string));
+                //ドロップされたデータをリストボックスに追加する
+                target.Items.Add(itemText);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string[] get_move = this.listBox1.Items.Cast<string>().ToArray();
+            var move = new List<int[]>();
+            int[,] ints = new int[2, 4] { { 0, 0, 1, -1 }, { -1, 1, 0, 0 } };
+            for (int i = 0; i < get_move.Length; i++)
+            {
+                if (get_move[i] == "up")
+                {
+                    move.Add(new int[2] { ints[0, 0], ints[1, 0] });
+                }
+                else if (get_move[i] == "down")
+                {
+                    move.Add(new int[2] { ints[0, 1], ints[1, 1] });
+                }
+                else if (get_move[i] == "right")
+                {
+                    move.Add(new int[2] { ints[0, 2], ints[1, 2] });
+                }
+                else if (get_move[i] == "left")
+                {
+                    move.Add(new int[2] { ints[0, 3], ints[1, 3] });
+                }
+            }
+        }
+
+        public bool Colision_detection(int x, int y, int[,] Map, List<int[]> move)
+        {
+            int max_x = Map.GetLength(0);
+            int max_y = Map.GetLength(1);
+
+            int new_x = x + move[0][0];
+            int new_y = y + move[0][1];
+
+            if ((new_x + 1) <= 0 || (max_x - new_x) <= 0 || (new_y + 1) <= 0 || (max_y - new_y) <= 0) return false;
+            else if (Map[new_x, new_y] == 0) return false;
+            else
+            {
+                //move.RemoveAt(0);
+                return true;
+            }
+        }
+
+        public void SquareMovement(int x, int y, int[,] Map, List<int[]> move)
+        {
+            while (Colision_detection(x, y, Map, move))
+            {
+                x += move[0][0];
+                y += move[0][1];
+                move.RemoveAt(0);
+            }
+        }
+    }
+
+}
